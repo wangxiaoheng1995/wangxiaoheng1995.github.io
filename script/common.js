99631f509b51d1ef845746e17a0886775726fc78
王小恒 <heng@xiaohengdeMacBook-Pro.local>
2019-03-08 18:10:32 +0800

    update

--- script/common.js
+++ script/common.js
@@ -1,26 +1,30 @@
 //本地获取
-function getStorage(str){
-   var data = localStorage.getItem(str)
-   if(data.indexOf("obj-") > -1){
-       return JSON.parse(data.slice(4));
-   }else{
-       return data.slice(4);
-   }
+function getStorage(key){
+   var data = localStorage.getItem(key);
+   return data;
 }
 //本地存贮
 function setStorage(key,data) {
-    if (typeof (data) == "string" || typeof (data) == "number") {
-        localStorage.setItem(key,"str-" + data);
-    } else {
-        localStorage.setItem(key,"obj-" + JSON.stringify(data));
+    var paramType = typeof(data);
+    if(paramType == 'object'){
+        data = JSON.stringify(data);
     }
+    localStorage.setItem(key,data);
 }
 //页面传参
 function pageParam () {
-  var paramStr = decodeURI(window.location.search).slice(1);
+  var paramStr = decodeURIComponent(window.location.search).slice(1);
   var paramObj = {};
   for (var i = 0; i < paramStr.split("&").length; i++) {
       paramObj[paramStr.split("&")[i].split("=")[0]] = paramStr.split("&")[i].split("=")[1];
   }
   return paramObj;
+}
+//检测用户是否登录
+var userInfo;
+function checkLogin() {
+    userInfo = JSON.parse(getStorage('userInfo'));
+    if(!userInfo){
+        location.href = 'login.html';
+    }
 }
--- script/config.js
+++ script/config.js
@@ -1,1 +1,1 @@
-var PHP_SERVE_URL = 'http://devmall.0571sd.com/';//平台服务端
+var PHP_SERVE_URL = 'http://mall.0571sd.com';//平台服务端
